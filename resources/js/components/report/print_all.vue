1f1d5f45d88314d434e22482d2f08a27274121ea
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2024-05-30 15:40:21 +0800

    esigna

--- resources/js/components/report/print_all.vue
+++ resources/js/components/report/print_all.vue
@@ -74,21 +74,20 @@
 						<div class="income-payments">{{ d.atc_remarks }}</div>
 						<div class="atc">{{ d.atc_code }}</div>
 						<div class="first-quarter">
-							
 							<span v-for="first in d.firstmonth">{{ first.amount }}<br></span>
 						</div>
-						<div class="first-total" >{{ d.subtotal_first }}</div>
+						<div class="first-total" v-if="d.firstmonth !=0">{{ d.subtotal_first }}</div>
 
 						<div class="second-quarter">
 							<span v-for="second in d.secondmonth">{{ second.amount }}<br></span>
 						</div>
-						<div class="second-total" >{{ d.subtotal_second }}</div>
+						<div class="second-total"  v-if="d.secondmonth !=0">{{ d.subtotal_second }}</div>
 
 						<div class="third-quarter">
 							<span v-for="third in d.thirdmonth">{{ third.amount }}<br></span>
 						</div>
-						<div class="third-total" >{{ d.subtotal_third }}</div>
-
+						<div class="third-total" v-if="d.thirdmonth !=0">{{ d.subtotal_third }}</div>
+						
 						<div class="sub-total" >
 							<span v-for="s in d.subtotal" >
 								<span v-if="s != 0">{{ parseFloat(s).toFixed(2) }}</span>
@@ -108,7 +107,7 @@
 						<div class="esig-container">
 							<img :src="'/images/'+d.accountant_signature" alt="" class="esignature">
 						</div>
-						
+
 						<div class="accountant"> {{ d.accountant_name }}</div>
 						<div class="accountant_position"> {{ d.accountant_position }}</div>
 						<div class="accountant_tin"> {{ d.accountant_tin }}</div>
